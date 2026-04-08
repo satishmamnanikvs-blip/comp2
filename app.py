fdc6b575ce5f2c31c1fb00a188a5dd6a330ecde9
satishmamnanikvs-blip <satishmamnanikvs@gmail.com>
2026-04-08 12:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ import torch.nn as nn
 from torchvision import transforms
 from PIL import Image, ImageOps
 import io
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="MNIST CNN API")
-
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+)
 
 # Device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
